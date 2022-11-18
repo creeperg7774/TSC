5be010c19255caa5b49a44e3464a52525c2a4f50
creeperg7774 <85454479+creeperg7774@users.noreply.github.com>
2022-11-18 12:41:05 -0600

    Update TSC.py

--- TSC.py
+++ TSC.py
@@ -1,22 +1,22 @@
 from math import *
 print(":|Please Enter Number1|: ")
-Number1=input()
-Operator=input(":|+ - / * or ≈ Exp²|: ")
+Number1 = input()
+Operator = input(":|+ - / *  or  | ≈ - UP or Down |  Exp² |: ")
 print(":|Please Enter Number2 :If Not */+- put 1|: ")
-Number2=input()
+Number2 = input()
 Number1 = float(Number1)
 Number2 = float(Number2)
 if Operator == "+":
-  Sum=Number1+Number2
+  Sum = Number1 + Number2
   print(Sum)
 if Operator == "-":
-  Sum=Number1-Number2
+  Sum = Number1 - Number2
   print(Sum)
 if Operator == "/":
-  Sum=Number1/Number2
+  Sum = Number1 / Number2
   print(Sum)
 if Operator == "*":
-  Sum=Number1*Number2
+  Sum = Number1 * Number2
   print(Sum)
 if Operator == "Up":
   Sum = floor(Number1)
@@ -24,9 +24,9 @@ if Operator == "Up":
 if Operator == "Down":
   Sum = ceil(Number1)
   print(Sum)
-if Operator == "Exp":
+if Operator == "Exp" or "exp" or "e" or "Exp²" or "exp²" or "Exp2" or "exp2" :
   print("Type the Exponent you would like: ")
-  Exponate=input()
-  Exponate=int(Exponate)
-  Sum = pow(Number1, Exponate)
+  Exponent = input()
+  Exponent = int(Exponent)
+  Sum = pow(Number1, Exponent)
   print(Sum)
